a91a0fecbd65c9f0b4001358fba31fff1dc576dc
Philomath88 <125642217+Philomath88@users.noreply.github.com>
2026-01-31 07:56:21 +0100

    Initial Commit

---
+++ PnPQRCode/PnPQRCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PnPQRCodeApp.swift
+//  PnPQRCode
+//
+//  Created by Maximilian Doelle on 31.01.26.
+//
+
+import SwiftUI
+
+@main
+struct PnPQRCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
